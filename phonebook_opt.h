ea26d80e83e10da2a491b5e6c5196161563e6a8b
yang196569 <yang196569@gmail.com>
2017-10-06 18:06:29 +0800

    English alphabets version

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -6,7 +6,7 @@
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
 // #define OPT 1
-typedef struct __PHONE_BOOK_ENTRY {
+typedef struct MORE_INFO {
     char lastName[MAX_LAST_NAME_SIZE];
     char firstName[16];
     char email[16];
@@ -17,6 +17,11 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
+    struct MORE_INFO *pNext;
+} Info;
+
+typedef struct __PHONE_BOOK_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
     struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
 
