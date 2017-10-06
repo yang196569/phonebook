88d21e9c1ffd520edef6366dc9bcdf04a55129a6
yang196569 <yang196569@gmail.com>
2017-10-06 20:00:25 +0800

    Shrinked entry version

---
+++ phonebook_opt_1.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
+#include "phonebook_opt.h"
+
+/* TODO: FILL YOUR OWN IMPLEMENTATION HERE! */
+
+entry *findName(char lastName[], entry *pHead)
+{
+    while (pHead != NULL) {
+        if (strcasecmp(lastName, pHead->lastName) == 0)
+            return pHead;
+        pHead = pHead->pNext;
+    }
+    return NULL;
+}
+
+entry *append(char lastName[], entry *e)
+{
+    e->pNext = (entry *) malloc(sizeof(entry));
+    e = e->pNext;
+    strcpy(e->lastName, lastName);
+    e->pNext = NULL;
+
+    return e;
+}
---
+++ phonebook_opt_1.h
@@ -0,0 +1,31 @@
+#ifndef _PHONEBOOK_H
+#define _PHONEBOOK_H
+
+#define MAX_LAST_NAME_SIZE 16
+
+/* TODO: After modifying the original version, uncomment the following
+ * line to set OPT properly */
+// #define OPT 1
+typedef struct MORE_INFO {
+    char lastName[MAX_LAST_NAME_SIZE];
+    char firstName[16];
+    char email[16];
+    char phone[10];
+    char cell[10];
+    char addr1[16];
+    char addr2[16];
+    char city[16];
+    char state[2];
+    char zip[5];
+    struct MORE_INFO *pNext;
+} Info;
+
+typedef struct __PHONE_BOOK_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
+    struct __PHONE_BOOK_ENTRY *pNext;
+} entry;
+
+entry *findName(char lastName[], entry *pHead);
+entry *append(char lastName[], entry *e);
+
+#endif
