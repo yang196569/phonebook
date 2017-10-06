1f0fe8222c569157dcca30237077a14639dabe1e
yang196569 <yang196569@gmail.com>
2017-10-06 18:08:45 +0800

    English alphabets version

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -1,16 +1,43 @@
 #include <stdlib.h>
-
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
 #include "phonebook_opt.h"
 
 /* TODO: FILL YOUR OWN IMPLEMENTATION HERE! */
+entry **entryList = NULL;
+entry **entryListHead = NULL;
+
 entry *findName(char lastName[], entry *pHead)
 {
-    /* TODO: implement */
+    pHead=*(entryListHead+lastName[0]-97);
+    while (pHead != NULL) {
+        if (strcasecmp(lastName, pHead->lastName) == 0)
+            return pHead;
+        pHead = pHead->pNext;
+    }
     return NULL;
 }
 
 entry *append(char lastName[], entry *e)
 {
-    /* TODO: implement */
-    return NULL;
+    /* allocate memory for the new entry and put lastName */
+    if(entryList==NULL) {
+        entryList = (entry **) malloc(sizeof(entry *)*26);
+        entryListHead= (entry **) malloc(sizeof(entry *)*26);
+    }
+    e = *( entryList+ lastName[0]-97);
+
+    if( *(entryListHead + lastName[0]-97) == NULL) {
+        e=(entry *)malloc(sizeof(entry));
+        * (entryListHead + lastName[0]-97 ) = e;
+    }
+
+
+    e->pNext = (entry *) malloc(sizeof(entry));
+    e = e->pNext;
+    strcpy(e->lastName, lastName);
+    e->pNext = NULL;
+    *(entryList + lastName[0] - 'a') = e;
+    return e;
 }
